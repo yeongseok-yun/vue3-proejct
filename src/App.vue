0a2285efa59a3ffab0ec8800dbf923553e2e5c4b
yun <antiravesol@gmail.com>
2023-07-22 15:53:35 +0900

    체크박스바인딩

--- src/App.vue
+++ src/App.vue
@@ -3,34 +3,48 @@
     <h2>To-Do List</h2>
     <form 
     @submit.prevent="onSubmit"
-      class="d-flex"
+      
       >
-      <div class="flex-grow-1 mr-2">
-        <input 
-          class="form-control"
-          type="text" 
-          v-model = "todo"
-          placeholder = "Type new to-do"
-        >
+      <div class="d-flex">
+        <div class="flex-grow-1 mr-2">
+          <input 
+            class="form-control"
+            type="text" 
+            v-model = "todo"
+            placeholder = "Type new to-do"
+          >
+        </div>
+        <div>
+          <button 
+            class = "btn btn-primary"
+            type="submit"
+          >
+          추가
+          </button>
+        </div>
       </div>
-      <div>
-        <button 
-          class = "btn btn-primary"
-          type="submit"
-        >
-        추가
-        </button>
-      </div>
-
+      
+      <div v-show = "hasError" style="color:red;">This field cannot be empty.</div>
     </form>
-    {{todos }}
     <div 
       class="card mt-2" 
       v-for="todo in todos" 
       :key="todo.id"
     >
       <div class="card-body p-2">
-        {{ todo.subject }}
+        <div class="form-check">
+          <input 
+            class="form-check-input"
+            type="checkbox"
+            v-model = "todo.completed"  
+          >
+          <label 
+            class="form-check-label"
+            for=""
+          >
+            {{ todo.subject}} 
+          </label>
+        </div>
       </div>
     </div>
   </div>
@@ -41,21 +55,22 @@ import { ref } from 'vue';
 export default {
   setup(){
     const todo = ref('');
-    const todos = ref([
-      {
-        id:1,subject:'휴대폰 사기'
-      },
-      {
-        id:2,subject:'장보기'
-
-      }
-    ]);
+    const todos = ref([]);
+    const hasError = ref(false);
     
     const onSubmit = () => {
-      todos.value.push({
-        id: Date.now(),
-        subject : todo.value
-      })
+      if(todo.value === ''){
+        hasError.value = true
+      }else{
+        todos.value.push({
+          id: Date.now(),
+          subject : todo.value,
+          completed : false,
+        })
+        hasError.value = false
+        todo.value = ''
+      }
+      
     }
 
 
@@ -63,6 +78,7 @@ export default {
       todo,
       todos,
       onSubmit,
+      hasError
     }
   }
 }
